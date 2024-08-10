ba07bb17a3714b7459d27eb708e30ccfaea92e8f
admin <samuvin.j@gmail.com>
2024-08-10 15:56:55 +0530

    Update

--- src/container/Main/LogoComponent.js
+++ src/container/Main/LogoComponent.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Logo.css";
 
-const LogoComponent = (props) => {
-	const theme = props.theme === "dark" ? "dark" : "light";
-
+const LogoComponent = () => {
+	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	useEffect(() => {
+		const savedTheme = localStorage.getItem("theme");
+		if (savedTheme === "dark") {
+			document.body.classList.add("dark-theme");
+			setIsDarkTheme(true);
+		}
+	}, []);
+	const changeTheme = () => {
+		setIsDarkTheme(!isDarkTheme);
+		if (!isDarkTheme) {
+			document.body.classList.add("dark-theme");
+			localStorage.setItem("theme", "dark");
+		} else {
+			document.body.classList.remove("dark-theme");
+			localStorage.setItem("theme", "light");
+		}
+	};
 	return (
-		<h1 className="logo" data-theme={theme}>
+		<h1 className="logo" onClick={changeTheme}>
 			SJ
 		</h1>
 	);
